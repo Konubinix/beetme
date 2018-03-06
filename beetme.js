f36580df28c941931c61ac13ce38da77ff432ad3
Samuel Loury <konubinixweb@gmail.com>
2018-03-06 22:56:02 +0100

    Remove the useless install hook

--- beetme.js
+++ beetme.js
@@ -1,15 +1,3 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open("beetme-offline").then(function(cache) {
-      return cache.addAll(
-        [
-          '/beetme.html',
-          '/beetme.js',
-          '/beetme.png',
-          '/beetme.json'
-        ]
-      );
-
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.open('beetme-offline').then(
@@ -18,7 +6,7 @@ self.addEventListener('fetch', function(event) {
 					return response || fetch(event.request).then(function(response) {
 						if(
 							event.request.url.match(new RegExp("/beetme.html|/beetme.png|/beetme.json"))
-						  )
+						)
 						{
 							cache.put(event.request, response.clone());
 						}
