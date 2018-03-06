ec46a6102f0e3b4aea60e8780cfba4dec7795258
Samuel Loury <konubinixweb@gmail.com>
2018-03-06 15:36:44 +0100

    Cache on fetch as well as on install
    This allows the update to be effective, reseting the cache.

--- beetme.js
+++ beetme.js
@@ -9,6 +9,22 @@ self.addEventListener('install', function(event) {
           '/beetme.json'
         ]
       );
-    })
-  );
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.open('beetme-offline').then(
+			function(cache) {
+				return cache.match(event.request).then(function (response) {
+					return response || fetch(event.request).then(function(response) {
+						if(
+							event.request.url.match(new RegExp("/beetme.html|/beetme.png|/beetme.json"))
+						  )
+						{
+							cache.put(event.request, response.clone());
+						}
+						return response;
+					});
+				});
+			})
+	);
 });
